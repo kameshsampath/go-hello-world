f914700200335473ba5d4dee236115b845d8d07b
Kamesh <kamesh.sampath@hotmail.com>
2022-10-25 08:27:22 +0530

    added test case

--- server.go
+++ server.go
@@ -6,11 +6,15 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+const greeting = "Hello, World!"
+
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", sayHello)
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func sayHello(c echo.Context) error {
+	return c.String(http.StatusOK, greeting)
 }
--- server_test.go
+++ server_test.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHelloWorld(t *testing.T) {
-	assert.True(t, true)
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	// Assertions
+	if assert.NoError(t, sayHello(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		got := rec.Body.String()
+		assert.Equalf(t, greeting, got, "Expecting %s but got %s", greeting, got)
+	}
 }
